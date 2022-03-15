17886338d3863f2b94cb6e8431471b8a65f806a6
brucebat <brucewatchbatman@163.com>
2022-03-15 10:15:59 +0800

    feat(src): 新增测试类

---
+++ src/main/java/com/brucebat/smart/thread/pool/App.java
@@ -0,0 +1,15 @@
+package com.brucebat.smart.thread.pool;
+
+/**
+ * 测试类
+ *
+ * @author brucebat
+ * @version 1.0
+ * @since Created at 2022/3/15 12:09 AM
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("This is a test.");
+    }
+}
